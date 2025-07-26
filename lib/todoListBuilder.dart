1f55e0b13d1badc948fee5480cc0259abaff9e1b
Swayanshu <swayanshu19@gmail.com>
2025-07-26 14:32:28 +0530

    completed

--- lib/todoListBuilder.dart
+++ lib/todoListBuilder.dart
@@ -109,7 +109,7 @@ class _TodolistbuilderState extends State<Todolistbuilder> {
                    builder: (context){
                 return Container(
                   padding: EdgeInsets.all(20),
-                  height: 140,
+                  height: 180,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
